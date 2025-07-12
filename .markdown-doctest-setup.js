72ab84578da706cf523b8cf1546a1d7b9dd38a5a
Morteza Ziyaeimehr <morteza.zym@gmail.com>
2025-07-12 22:41:24 +0200

    restore pre-commit hooks

--- .markdown-doctest-setup.js
+++ .markdown-doctest-setup.js
@@ -1,9 +1,9 @@
-export default {
-    babel: false,
-    require: {
-        "persian-date": require("./dist/persian-date.js"),
-    },
-    globals: {
-        persianDate: require("./dist/persian-date.js"),
-    },
+module.exports = {
+	babel: false,
+	require: {
+		"persian-date": require("./dist/persian-date.js"),
+	},
+	globals: {
+		persianDate: require("./dist/persian-date.js"),
+	},
 };
